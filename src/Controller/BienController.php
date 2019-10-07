f2d9f552d923d16e9b09d6b2afa7fbccd68bc2d5
etudiant <etudiant@debian9-gui.sio.lla.fr>
2019-10-07 18:01:49 +0200

    Visite à terminer

--- src/Controller/BienController.php
+++ src/Controller/BienController.php
@@ -36,9 +36,66 @@ class BienController extends AbstractController
      */
     public function consulterBien(Request $query)
     {
-        return $this->render('bien/consulterBien.html.twig', array('lesBiens' => $this->getBiens()));
+        return $this->render('bien/consulterBien.html.twig', array('lesBiens' => $this->getBiens(), 'found' => 0));
     }
-        
+    
+    /**
+     * @Route("/modifierBien/{id}", name="modifierBien")
+     */
+    public function modifierBien(Request $query, int $id)
+    {
+        $lesBiens = $this->getBiens();
+        foreach ($lesBiens as $bien) {
+            if ($bien->getId() == $id) {
+                $bienModifie = new Bien();
+                $form = $this->createForm(BienType::class, $bienModifie);
+                $form->handleRequest($query);
+                if ($query->isMethod('POST')) {
+                    if ($form->isValid()) {
+                        $em = $this->getDoctrine()->getManager();
+                        $bien->setNbPiece($form['nb_piece']->getData());
+                        $bien->setNbChambre($form['nb_chambre']->getData());
+                        $bien->setSuperficie($form['superficie']->getData());
+                        $bien->setPrix($form['prix']->getData());
+                        $bien->setChauffage($form['chauffage']->getData());
+                        $bien->setAnnee($form['annee']->getData());
+                        $bien->setLocalisation($form['localisation']->getData());
+                        $bien->setEtat($form['etat']->getData());
+                        $bien->setIdType($form['id_type']->getData());
+                        $em->flush();
+                        return $this->render('bien/modifierBien.html.twig', array('bien' => $bien, 'form' => $form->createView(), 'success' => 1)); 
+                    }
+                }
+                return $this->render('bien/modifierBien.html.twig', array('bien' => $bien, 'form' => $form->createView(), 'success' => 0));
+            }
+        }
+        return $this->render('bien/consulterBien.html.twig', array('lesBiens' => $lesBiens, 'found' => -1));
+    }
+    
+    /**
+     * @Route("/supprimerBien/{id}", name="supprimerBien")
+     */
+    public function supprimerBien(Request $query, int $id)
+    {
+        return $this->render('bien/supprimerBien.html.twig', array('id' => $id));
+    }
+    
+    /**
+     * @Route("/suppressionBien/{id}", name="suppressionBien")
+     */
+    public function suppressionBien(Request $query, int $id)
+    {
+        $lesBiens = $this->getBiens();
+        foreach ($lesBiens as $bien) {
+            if ($bien->getId() == $id) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($bien);
+                $em->flush();
+            }
+        }
+        return $this->render('bien/consulterBien.html.twig', array('lesBiens' => $this->getBiens(), 'found' => 0));
+    }
+    
     /**
      * @Route("/ajouterTypeBien", name="ajouterTypeBien")
      */
@@ -62,6 +119,38 @@ class BienController extends AbstractController
             }
         }
         return $this->render('bien/ajouterTypeBien.html.twig', array('form' => $form->createView(), 'success' => 0));
+    }
+    
+    /**
+     * @Route("/consulterTypesBien", name="consulterTypesBiens")
+     */
+    public function consulterTypesBien(Request $query)
+    {
+        return $this->render('bien/consulterTypesBien.html.twig', array('lesTypes' => $this->getTypes(), 'found' => 0));
+    }
+    
+    /**
+     * @Route("/supprimerType/{id}", name="supprimerType")
+     */
+    public function supprimerType(Request $query, int $id)
+    {
+        return $this->render('bien/supprimerType.html.twig', array('id' => $id));
+    }
+    
+    /**
+     * @Route("/suppressionType/{id}", name="suppressionType")
+     */
+    public function suppressionType(Request $query, int $id)
+    {
+        $lesTypes = $this->getTypes();
+        foreach ($lesTypes as $type) {
+            if ($type->getId() == $id) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($type);
+                $em->flush();
+            }
+        }
+        return $this->render('bien/consulterTypesBien.html.twig', array('lesTypes' => $this->getTypes(), 'found' => 0));
     }
     
     public function getTypes() {
--- src/Controller/VisiteurController.php
+++ src/Controller/VisiteurController.php
@@ -35,6 +35,66 @@ class VisiteurController extends AbstractController
         return $this->render('visiteur/ajouterVisiteur.html.twig', array('form' => $form->createView(), 'success' => 0));
     }
     
+    /**
+     * @Route("/consulterVisiteurs", name="consulterVisiteurs")
+     */
+    public function consulterVisiteurs(Request $query)
+    {
+        return $this->render('visiteur/consulterVisiteurs.html.twig', array('lesVisiteurs' => $this->getVisiteurs(), 'found' => 0));
+    }
+    
+    /**
+     * @Route("/modifierVisiteur/{id}", name="modifierVisiteur")
+     */
+    public function modifierVisiteur(Request $query, int $id)
+    {
+        $lesVisiteurs = $this->getVisiteurs();
+        foreach ($lesVisiteurs as $visiteur) {
+            if ($visiteur->getId() == $id) {
+                $visiteurModifie = new Visiteur();
+                $form = $this->createForm(VisiteurType::class, $visiteurModifie);
+                $form->handleRequest($query);
+                if ($query->isMethod('POST')) {
+                    if ($form->isValid()) {
+                        $em = $this->getDoctrine()->getManager();
+                        $visiteur->setNom($form['nom']->getData());
+                        $visiteur->setPrenom($form['prenom']->getData());
+                        $visiteur->setAdresse($form['adresse']->getData());
+                        $visiteur->setTelephone($form['telephone']->getData());
+                        $em->flush();
+                        return $this->render('visiteur/modifierVisiteur.html.twig', array('visiteur' => $visiteur, 'form' => $form->createView(), 'success' => 1)); 
+                    }
+                }
+                return $this->render('visiteur/modifierVisiteur.html.twig', array('visiteur' => $visiteur, 'form' => $form->createView(), 'success' => 0));
+            }
+        }
+        return $this->render('visiteur/consulterVisiteurs.html.twig', array('lesVisiteurs' => $lesVisiteurs, 'found' => -1));
+    }
+    
+    /**
+     * @Route("/supprimerVisiteur/{id}", name="supprimerVisiteur")
+     */
+    public function supprimerVisiteur(Request $query, int $id)
+    {
+        return $this->render('visiteur/supprimerVisiteur.html.twig', array('id' => $id));
+    }
+    
+    /**
+     * @Route("/suppressionVisiteur/{id}", name="suppressionVisiteur")
+     */
+    public function suppressionVisiteur(Request $query, int $id)
+    {
+        $lesVisiteurs = $this->getVisiteurs();
+        foreach ($lesVisiteurs as $visiteur) {
+            if ($visiteur->getId() == $id) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($visiteur);
+                $em->flush();
+            }
+        }
+        return $this->render('visiteur/consulterVisiteurs.html.twig', array('lesVisiteurs' => $this->getVisiteurs(), 'found' => 0));
+    }
+    
     public function getVisiteurs() {
         $lesVisiteurs = $this->getDoctrine()->getRepository(\App\Entity\Visiteur::class)->findAll();
         return $lesVisiteurs;
