cad04f11e3526e3a5358688353bf380ee7d7dfa2
etudiant <etudiant@debian9-gui.sio.lla.fr>
2019-10-07 18:59:56 +0200

    Projet terminé

--- src/Controller/VisiteController.php
+++ src/Controller/VisiteController.php
@@ -35,6 +35,65 @@ class VisiteController extends AbstractController
         return $this->render('visite/ajouterVisite.html.twig', array('form' => $form->createView(), 'success' => 0));
     }
     
+    /**
+     * @Route("/consulterVisites", name="consulterVisites")
+     */
+    public function consulterVisites(Request $query)
+    {
+        return $this->render('visite/consulterVisites.html.twig', array('lesVisites' => $this->getVisites(), 'found' => 0));
+    }
+    
+    /**
+     * @Route("/modifierVisite/{id}", name="modifierVisite")
+     */
+    public function modifierVisite(Request $query, int $id)
+    {
+        $lesVisites = $this->getVisites();
+        foreach ($lesVisites as $visite) {
+            if ($visite->getId() == $id) {
+                $visiteModifie = new Visite();
+                $form = $this->createForm(VisiteType::class, $visiteModifie);
+                $form->handleRequest($query);
+                if ($query->isMethod('POST')) {
+                    if ($form->isValid()) {
+                        $em = $this->getDoctrine()->getManager();
+                        $visite->setIdVisiteur($form['id_visiteur']->getData());
+                        $visite->setIdBien($form['id_bien']->getData());
+                        $visite->setSuite($form['suite']->getData());
+                        $em->flush();
+                        return $this->render('visite/modifierVisite.html.twig', array('visite' => $visite, 'form' => $form->createView(), 'success' => 1)); 
+                    }
+                }
+                return $this->render('visite/modifierVisite.html.twig', array('visite' => $visite, 'form' => $form->createView(), 'success' => 0));
+            }
+        }
+        return $this->render('visite/consulterVisites.html.twig', array('lesVisites' => $lesVisites, 'found' => -1));
+    }
+    
+    /**
+     * @Route("/supprimerVisite/{id}", name="supprimerVisite")
+     */
+    public function supprimerVisite(Request $query, int $id)
+    {
+        return $this->render('visite/supprimerVisite.html.twig', array('id' => $id));
+    }
+    
+    /**
+     * @Route("/suppressionVisite/{id}", name="suppressionVisite")
+     */
+    public function suppressionVisite(Request $query, int $id)
+    {
+        $lesVisites = $this->getVisites();
+        foreach ($lesVisites as $visite) {
+            if ($visite->getId() == $id) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($visite);
+                $em->flush();
+            }
+        }
+        return $this->render('visite/consulterVisites.html.twig', array('lesVisites' => $this->getVisites(), 'found' => 0));
+    }
+    
     public function getVisites() {
         $lesVisites = $this->getDoctrine()->getRepository(\App\Entity\Visite::class)->findAll();
         return $lesVisites;
